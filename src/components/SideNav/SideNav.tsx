19b61e9abbfcfc4dc58aa631d165d047d32173bf
Kevin Shi <kevin.shi@shopify.com>
2021-04-18 13:38:10 -0400

    Fix sidenav animation

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import {CSSTransition} from 'react-transition-group';
+// import {CSSTransition} from 'react-transition-group';
 import {classNames} from '../../utilities';
 import styles from './SideNav.scss';
-import sideNavTransition from './SideNavTransition.scss';
+// import sideNavTransition from './SideNavTransition.scss';
 
 interface Props {
   open: boolean;
-  unmountOnClose?: boolean;
 }
 
-const SideNav: React.FC<Props> = ({children, open, unmountOnClose = false}) => (
-  <CSSTransition
-    key="side-nav-key"
-    in={open}
-    classNames={sideNavTransition}
-    timeout={250}
-    unmountOnExit={unmountOnClose}
-    addEndListener={() => {}}
+const SideNav: React.FC<Props> = ({children, open}) => (
+  // <CSSTransition
+  //   key="side-nav-key"
+  //   in={open}
+  //   classNames={sideNavTransition}
+  //   timeout={250}
+  //   unmountOnExit={unmountOnClose}
+  //   addEndListener={() => {}}
+  // >
+  <nav
+    key="side-nav"
+    className={classNames({
+      [styles.SideNav]: true,
+      [styles.Open]: open,
+    })}
   >
-    <nav
-      key="side-nav"
-      className={classNames({
-        [styles.SideNav]: true,
-        [styles.Open]: open || unmountOnClose,
-      })}
-    >
-      <div className={styles.Content}>{children}</div>
-    </nav>
-  </CSSTransition>
+    <div className={styles.Content}>{children}</div>
+  </nav>
+  // </CSSTransition>
 );
 
 export default SideNav;
